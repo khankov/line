c04ad56b40e8f946577461d50e1ef4b3e4e3b09f
Edge <khankov1vo@gmail.com>
2022-12-16 00:27:01 +0300

    дз

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1(){
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+    public static void task2(){
+        System.out.println("Задание 2");
+        String fullname = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " +fullname.toUpperCase());
+
+    }
+    public static void task3(){
+        System.out.println("Задание 3");
+        String fullname = "Иванов Семён Семёнович";
+        fullname = fullname.replaceAll("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullname);
+
+    }
+}
